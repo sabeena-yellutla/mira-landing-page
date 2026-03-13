ac3c4dd56096c7ba5c0f480871e6eece94f69eba
Sabeena Yellutla <yellutlasabeena@gmail.com>
2026-03-13 21:32:36 +0530

    Refactored CSS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Read savedTheme from local storage on page load
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "true") {
     document.body.classList.add("dark");
 }
+
+// toggling theme by adding dark class to body, and store to local storage
 document.querySelector('.theme-toggle').addEventListener('click', () => {
     const theme = document.body.classList.toggle('dark');
     localStorage.setItem("theme", theme);
